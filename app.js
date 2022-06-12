2170f4c51c45e703e9f590820342b11e766abead
Marina Gutkevich <gutmalina@mail.ru>
2022-06-12 16:19:37 +0300

    schema and model

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Applistening on port ${PORT}`)
-})
+
+mongoose.connect('mongodb://localhost:27017/mestodb ', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+
+app.listen(PORT)
