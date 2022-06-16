c595609c6d8f0eb853b8fd566fc20463c0dc0759
Marina Gutkevich <gutmalina@mail.ru>
2022-06-16 19:48:25 +0300

    usersError

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const user = require('../models/user');
 const User = require('../models/user');
 
 /**получить всех пользователей */
@@ -19,7 +20,7 @@ module.exports.getUserById= async (req, res) => {
     res.status(200).send({ data: user });
   }catch(err){
     if(err.name === "CastError"){
-      res.status(400).send('Введен некорректный id пользователя');
+      res.status(400).send({data: user, message: 'Введен некорректный id пользователя'});
       return
     }else{
       res.status(500).send('Ошибка сервера')
