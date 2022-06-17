15f29d717f0f76b89e54a38ff67a76a091e0e883
Marina Gutkevich <gutmalina@mail.ru>
2022-06-17 20:35:20 +0300

    cards<Like

--- controllers/cards.js
+++ controllers/cards.js
@@ -52,28 +52,31 @@ module.exports.deleteCard = async (req, res)=>{
 };
 
 /** поставить лайк карточке */
-module.exports.likeCard = async (req, res)=>{
-  const cardId = req.params.cardId;
+module.exports.likeCard = (req, res)=>{
+  //const cardId = req.params.cardId;
+  const cardId = "6zac920b4e00b91d025a5af6";
   const userId = req.user._id;
-  try{
-    const card = await Card.findByIdAndUpdate(  cardId, { $addToSet: { likes: userId } }, { new: true })
-    if(!cardId){
-      res.status(404).send({message: 'Карточка с указанным id не найдена'})
-      return
-    }
-    res.status(200).send({ data: card })
-  }catch(err){
-    if(err.name === "ValidationError" || err.name === "CastError"){
-      res.status(400).send({message: 'Введены некорректные данные'});
-      return
-    }
-    res.status(500).send({message: 'Ошибка сервера'})
-  }
+  Card.findByIdAndUpdate(  cardId, { $addToSet: { likes: userId } }, { new: true })
+    .then((card)=>{
+      if(!card){
+        res.status(404).send({message: 'Карточка с указанным id не найдена'})
+        return
+      }
+      res.status(200).send({ data: card })
+    })
+    .catch((err)=>{
+      if(err.name === "ValidationError" || err.name === "CastError"){
+        res.status(400).send({ message: 'Введены некорректные данные'});
+        return
+      }
+      res.status(500).send({message: 'Ошибка сервера'})
+    })
 };
 
 /** удалить лайк у карточки */
 module.exports.dislikeCard = async (req, res)=>{
-  const cardId = req.params.cardId;
+  const cardId = "62ab1e0e9eb04686deff560c";
+  //const cardId = req.params.cardId;
   const userId = req.user._id;
   try{
     const card = await Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
@@ -84,9 +87,13 @@ module.exports.dislikeCard = async (req, res)=>{
       return
     }
     if(err.name === "ValidationError" || err.name === "CastError"){
-      res.status(400).send({message: 'Введены некорректные данные'});
+      res.status(400).send({err, message: 'Введены некорректные данные'});
       return
     }
     res.status(500).send({message: 'Ошибка сервера'})
   }
 };
+
+// "62ab1e0e9eb04686deff560c"
+// "62ac920b4e00b91d025a5af6"
+// "62ac92244e00b91d025a5af8"
