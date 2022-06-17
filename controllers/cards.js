fb551a772586be895a67cc5cadc651b1fd6a6402
Marina Gutkevich <gutmalina@mail.ru>
2022-06-17 15:04:44 +0300

    Cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,11 +1,10 @@
 const Card = require('../models/card');
-const ServerError = require('../Error/ServerError')
 
 /**получить все карточки */
 module.exports.getCards=(req, res) => {
   Card.find({})
     .then(card => res.status(200).send({ data: card }))
-    .catch((err) => {throw new ServerError({ message: 'Ошибка сервера'})});
+    .catch((err) => res.status(500).send({message: 'Ошибка сервера'}));
 };
 
 /** создать карточку */
@@ -14,49 +13,52 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
   Card.create({ name, link, owner })
     .then(card => res.status(200).send({ data: card }))
-    .catch((err) => {throw new ServerError({ message: 'Ошибка сервера'})});
+    .catch((err) => res.status(500).send({message: 'Ошибка сервера'}));
 };
 
 /** удалить карточку по ID */
 module.exports.deleteCard = (req, res)=>{
   const userId = req.user._id;
-  Card.findByIdAndRemove(req.params.CardId)
-    .then((card)=>{
-      if(!card) {
-        res.status(404).send('Карточка с указанным id не найдена')
-        return
-      }
-      if(String(userId) !== String(card.owner._id)){
-        res.status(403).send('Карточка не может быть удалена')
-        return
-      }
-      res.status(200).send({ data: card, message: "Карточка удалена" });
-    })
-    .catch((err) => {throw new ServerError('Ошибка сервера')});
+  try{
+    const card = Card.findByIdAndRemove(req.params.CardId)
+    if(!card) {
+      res.status(404).send({message: 'Карточка с указанным id не найдена'})
+      return
+    }
+    if(String(userId) !== String(card.owner._id)){
+      res.status(403).send({message: 'Карточка не может быть удалена'})
+      return
+    }
+    res.status(200).send({ data: card, message: "Карточка удалена" });
+  }catch(err){
+    res.status(500).send({message: 'Ошибка сервера'})
+  }
 };
 
 /** поставить лайк карточке */
 module.exports.likeCard = (req, res)=>{
-  Card.findByIdAndUpdate(  req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((card) =>{
-      if(!card){
-        res.status(404).send('Карточка с указанным id не найдена')
-        return
-      }
-      res.status(200).send({ data: card })
-    })
-    .catch((err) => {throw new ServerError('Ошибка сервера')});
+  try{
+    const card = Card.findByIdAndUpdate(  req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    if(!card){
+      res.status(404).send({message: 'Карточка с указанным id не найдена'})
+      return
+    }
+    res.status(200).send({ data: card })
+  }catch(err){
+    res.status(500).send({message: 'Ошибка сервера'})
+  }
 };
 
 /** удалить лайк у карточки */
 module.exports.dislikeCard = (req, res)=>{
-  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((card) => {
-      if(!card){
-        res.status(404).send('Карточка с указанным id не найдена')
-        return
-      }
-      res.status(200).send({ data: card })
-    })
-    .catch((err) => {throw new ServerError('Ошибка сервера')});
+  try{
+    const card = Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    if(!card){
+      res.status(404).send({message: 'Карточка с указанным id не найдена'})
+      return
+    }
+    res.status(200).send({ data: card })
+  }catch(err){
+    res.status(500).send({message: 'Ошибка сервера'})
+  }
 };
