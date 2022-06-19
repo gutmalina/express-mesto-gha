50da1f3c58587cdc18d0135b2c1817b0199c6dca
Marina Gutkevich <gutmalina@mail.ru>
2022-06-19 16:58:37 +0300

    project13-review2

--- controllers/users.js
+++ controllers/users.js
@@ -60,7 +60,7 @@ module.exports.updateUser = async (req, res) => {
   const { name, about } = req.body;
   const userId = req.user._id;
   try {
-    const user = await User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true }).orFail(() => new Error('Пользователь по указанному id не найден'));
+    const user = await User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true }).orFail(() => Error('Пользователь по указанному id не найден'));
     res.status(200).send({ user });
   } catch (err) {
     if (err.name === 'ValidationError' || err.name === 'CastError') {
