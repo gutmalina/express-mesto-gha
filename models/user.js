f5887ecae26624d99fb15a4e0c8ff2079d293341
Marina Gutkevich <gutmalina@mail.ru>
2022-06-19 16:25:21 +0300

    project13-review2

--- models/user.js
+++ models/user.js
@@ -4,13 +4,13 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
+    // minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
     required: true,
-    minlength: 2,
+    // minlength: 2,
     maxlength: 30,
   },
   avatar: {
