4db3fd65ae99e7b99c4e344a5a2d93e5aa417435
Marina Gutkevich <gutmalina@mail.ru>
2022-06-19 15:40:10 +0300

    project13-review1

--- controllers/users.js
+++ controllers/users.js
@@ -59,7 +59,7 @@ module.exports.createUser = (req, res) => {
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
   const userId = req.user._id;
-  User.findByIdAndUpdate(userId, { name, about }, { returnNewDocument: true, runValidators: true })
+  User.findByIdAndUpdate(userId, { name, about })
     .orFail(() => new Error('Пользователь по указанному id не найден'))
     .then((user) => {
       res.status(200).send({ data: user });
