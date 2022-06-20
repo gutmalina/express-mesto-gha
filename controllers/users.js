37b88ea3808e9f04a78c0b1aabb6d442a85a67e2
Marina Gutkevich <gutmalina@mail.ru>
2022-06-20 14:13:07 +0300

    project13-review2

--- controllers/users.js
+++ controllers/users.js
@@ -61,7 +61,8 @@ module.exports.updateUser = async (req, res) => {
   const { name, about } = req.body;
   const userId = req.user._id;
   try {
-    const user = await User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true }).orFail(() => Error('Пользователь по указанному id не найден'));
+    const user = await User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true });
+    // .orFail(() => Error('Пользователь по указанному id не найден'));
     res.status(200).send({ user });
   } catch (err) {
     if (err.name === 'ValidationError' || err.name === 'CastError') {
@@ -77,7 +78,8 @@ module.exports.updateAvatar = async (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
   try {
-    const user = await User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true }).orFail(() => Error('Пользователь по указанному id не найден'));
+    const user = await User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true });
+    // .orFail(() => Error('Пользователь по указанному id не найден'));
     res.status(200).send({ avatar: user.avatar });
   } catch (err) {
     if (err.name === 'ValidationError' || err.name === 'CastError') {
