98e49f799b6b9118926463c6c8464328c934f2f2
Marina Gutkevich <gutmalina@mail.ru>
2022-06-27 20:36:49 +0300

    joi createuser

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,12 +1,14 @@
 const { celebrate, Joi } = require('celebrate');
 
+const regexLink = /^https?:\/\/\w\S{1,}#?$/gi;
+
 const validateCreateUser = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().length(10),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().required().regex(RegExp(regexLink)),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().length(10),
   }),
 });
 
