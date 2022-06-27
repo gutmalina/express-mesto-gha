f1ac739e9aa5a0555558db0f039cb90dbb955d02
Marina Gutkevich <gutmalina@mail.ru>
2022-06-27 20:27:18 +0300

    joi createuser

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -2,8 +2,11 @@ const { celebrate, Joi } = require('celebrate');
 
 const validateCreateUser = celebrate({
   body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().length(10),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
   }),
 });
 
