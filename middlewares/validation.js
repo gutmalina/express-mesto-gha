51ef22bc112f21159126aed9ca089d95d7648d3c
Marina Gutkevich <gutmalina@mail.ru>
2022-06-30 12:57:13 +0300

    remove validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -24,7 +24,7 @@ const validateLogin = celebrate({
 /** получить пользователя по ID */
 const validateGetUserById = celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().length(24).hex(),
+    userId: Joi.string(),
   }),
 });
 
@@ -56,21 +56,21 @@ const validateCreateCard = celebrate({
 /** удалить карточку по ID */
 const validateDeleteCard = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex(),
+    cardId: Joi.string(),
   }),
 });
 
 /** поставить лайк карточке */
 const validateLikeCard = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex(),
+    cardId: Joi.string(),
   }),
 });
 
 /** удалить лайк у карточки */
 const validateDislikeCard = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex(),
+    cardId: Joi.string(),
   }),
 });
 
